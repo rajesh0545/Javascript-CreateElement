30ef33d70298eb6afa5bd68dec833c916ead8ff7
rajesh <krajeshgowri7032@mail.com>
2022-08-20 23:08:20 +0530

    Javascript-create Element

---
+++ script.js
@@ -0,0 +1,9 @@
+function Element(){
+     var x = document.createElement("button");
+     x.innerHTML="Kanchikunta";
+     document.body.appendChild(x)
+     document.getElementById("create").style.color = "red";
+
+}
+
+
